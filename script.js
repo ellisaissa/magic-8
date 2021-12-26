f2baeeaa974af9ac3b06eaa46cce6aacc3bd36a1
ellisa <ellisaissa@gmail.com>
2021-12-25 21:43:19 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+Math.floor(Math.random() * 4);
+switch ("") {
+    case 1:"try again"
+    break;
+    case 2:"inconclusive"
+    break;
+    case 3: "Maybe"
+    break;
+    case 4:"It could be"
+    break;
+    case 5:"Don't count on it"
+    break;
+    case 6: "You may rely on it"
+    break;
+    case 7:"Reply hazy, try again"
+    break;
+}
